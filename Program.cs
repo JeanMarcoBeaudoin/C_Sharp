7e731bb6edcdd5a13a2d3cf3c4803eddb83f8850
Jean-Marc Beaudoin <jeanmarcobeaudoin@gmail.com>
2024-04-15 15:43:51 -0400

    inti

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
+        int current = 0;
+
+        do
+        {
+
+            current = random.Next(1, 11);
+            Console.WriteLine(current); 
+        } while (current != 7);
+
+        while (current != 7)
+        {
+
+            Console.WriteLine(current); 
+            current = random.Next(1,11);
+        };
+    }
+}
